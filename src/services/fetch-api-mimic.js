e7998d92fc6f8cf2f7029ce462334ebb03d2f92d
arjunpalat <palathinkaraarjun@gmail.com>
2024-03-30 22:52:29 +0530

    Update pagination scroll logic based on mimic API

--- src/services/fetch-api-mimic.js
+++ src/services/fetch-api-mimic.js
@@ -1,12 +1,18 @@
-import { MimicLogs } from "@mimic/mimic-logs";
+import { MimicLogs } from "./api-mimic";
 
-export const fetchLogs = async (logs, tsDifference) => {
-  const offsetTs = logs.length > 0 ? logs[0].timestamp : Date.now();
-  const previousLogs = (
-    await MimicLogs.fetchPreviousLogs({
-      startTs: offsetTs - tsDifference,
-      endTs: offsetTs,
-      limit: 100,
-    })
-  ).reverse();
+export const fetcherLogs = async (logs, tsDifference) => {
+  try {
+    const offsetTs = logs.length > 0 ? logs[0].timestamp : Date.now();
+    const previousLogs = (
+      await MimicLogs.fetchPreviousLogs({
+        startTs: offsetTs - tsDifference / 10,
+        endTs: offsetTs,
+        limit: 10,
+      })
+    ).reverse();
+    return previousLogs;
+  } catch (error) {
+    console.error("Error fetching previous logs:", error);
+    return null;
+  }
 };
